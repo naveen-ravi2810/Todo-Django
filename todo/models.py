1f781e7f646b443ecd7ed2e881d47cdc594a667c
naveen-ravi2810 <rnaveen28102003@gmail.com>
2024-06-05 13:33:24 +0530

    First commit

---
+++ todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from uuid import uuid4
+from datetime import datetime
+from user.models import Users
+
+
+# Create your models here.
+class Todos(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    task = models.TextField(max_length=100)
+    status = models.BooleanField(default=False)
+    created_on = models.DateTimeField(default=datetime.now())
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
---
+++ user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from uuid import uuid4
+
+
+# Create your models here.
+class Users(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    first_name = models.TextField(max_length=20)
+    last_name = models.TextField(max_length=20)
+    email = models.EmailField(max_length=60)
+    password = models.TextField(max_length=300)  
